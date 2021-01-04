1ccdb484b3b22ac3e077912081de4baf6accc5c8
Amanda Nelson <amanda.gaylord@gmail.com>
2021-01-03 20:36:10 -0500

    Reducer.js updated with better reducer case functions.

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,37 +1,10 @@
 import React, { useState } from "react";
+import {todoReducer, initialState} from "../reducers/Reducer";
+import Todo from "./Todo";
 
-const TodoForm = ({ dispatch }) => {
-  const [todo, setTodo] = useState("");
+function TodoForm() {
+  const 
 
-  const handleChanges = (event) => {
-    setTodo(event.target.value);
-  };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    dispatch({
-      type: "ADD_TODO",
-      payload: todo,
-    });
-    setTodo("");
-  };
-  const handleClear = (event) => {
-    event.preventDefault();
-    dispatch({
-      type: "CLEAR_COMPLETED",
-    });
-  };
-  return (
-    <div>
-      <form className="addForm" onSubmit={handleSubmit}>
-        <input placeholder="To-Do..." onChange={handleChanges} value={todo} />
-        <button className="addNew">Add</button>
-        <br />
-      </form>
-      <button className="clear" onClick={handleClear}>
-        Clear Completed
-      </button>
-    </div>
-  );
-};
+}
 
 export default TodoForm;
--- src/reducers/Reducer.js
+++ src/reducers/Reducer.js
@@ -1,33 +1,38 @@
 import redux from "redux";
 
-const todo = [
-  {
-    todo: "example: Do Laundry",
-    id: 1528817077286,
-    completed: true,
-  },
-  {
-    todo: "example: empty dishwasher",
-    id: 1528817084358,
-    completed: true,
-  },
-];
+const initialState = {
+  todoList: [
+    {
+      todo: "example: Do Laundry",
+      id: 1528817077286,
+      completed: true,
+    },
+    {
+      todo: "example: empty dishwasher",
+      id: 1528817084358,
+      completed: true,
+    },
+  ],
+};
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_TODO":
-      return [
-        ...state,
-        {
-          todo: action.payload,
-          id: Date.now(),
-          completed: false,
-        },
-      ];
+      return { ...state, todoList: [...state.todoList, action.payload] };
     case "TOGGLE_COMPLETE":
-      return state.map((todo) => {});
+      return {
+        ...state,
+        todoList: state.todoList.map((item) =>
+          item.id === action.id ? { ...item, completed: !item.completed } : item
+        ),
+      };
+    case "CLEAR_COMPLETED":
+      return {
+        ...state,
+        todoList: state.todoList.filter((item) => item.completed === false),
+      };
     default:
       return state;
   }
 };
-export default { reducer, todo };
+export default { reducer, initialState };
